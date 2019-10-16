e2075fb3785aa6d6e69cd7b3d4e78b8c0713e6bc
Victor_Li <lytz@berkeley.edu>
2019-10-15 17:04:38 -0700

    auth

--- Webdroid/GlobalConstants.js
+++ Webdroid/GlobalConstants.js
@@ -1,5 +1,5 @@
 export default {
   API_BASE_URL: "https://api.eventure-app.com/",
-  USERNAME: "Eventure 1.0.4",
-  PASSWORD: "efd2d743b153e71e6244b2ebf4bb9e75"
+  USERNAME: "Eventure 1.0.6",
+  PASSWORD: "5223c7b37ae1ee6a1ee1f6fa2bf95214"
 };
